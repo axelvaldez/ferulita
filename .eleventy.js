805b4da5c9e9a603744c8ea07b81cce3e5b9b6ed
Axel Valdez <axel@paskola.com>
2023-09-19 09:39:23 -0700

    cleanup; half-ass new build script

--- .eleventy.js
+++ .eleventy.js
@@ -10,9 +10,12 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy({ "node_modules/shikwasa/dist": "assets/shikwasa" });
-    eleventyConfig.addWatchTarget("./_sass/");
+    // eleventyConfig.addWatchTarget("./_sass/");
     eleventyConfig.addFilter("debugger", (...args) => {
         console.log(...args)
         debugger;
-    })
+    });
+    // eleventyConfig.on('eleventy.after', async ({ dir, results, runMode, outputMode }) => {
+    //     eleventyConfig.addPassthroughCopy({ "pages/*": "episodios/" });
+    // });
 };
